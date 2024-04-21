28f8d3c06eba5c558126a2468a5594d721a80d94
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-04-21 14:20:25 -0400

    Use a `NameTable`.

--- src/types.ml
+++ src/types.ml
@@ -2,8 +2,10 @@ module FamilyId = struct
   type t = int 
 end 
 
+module NameTable = Map.Make(Names.Id)
+
 module FamilyName = struct 
-  type t = Names.Id.t * FamilyId.t 
+  type t = { name: Names.Id.t; id: FamilyId.t }
 end 
 
 (* The type of an "element" in a family *)
@@ -12,14 +14,17 @@ module rec FamilyBodyElemTy : sig
 end = FamilyBodyElemTy
 
 (* The family context, binding names to their respective types *)
-and FamilyContext : sig 
-  type t = (Names.Id.t * FamilyType.t) list
+and FamilyContext : sig
+  type t = FamilyType.t NameTable.t
 end = FamilyContext
 
 (* The "type" of a family *)
 and FamilyType : sig
   type t =
     { name : FamilyName.t; 
-      body : (Names.Id.t * FamilyBodyElemTy.t) list; }
+      body : FamilyBodyElemTy.t NameTable.t; }
 
 end = FamilyType
+
+
+
