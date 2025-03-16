9de94c2c330e8b49bd6b5138d60b1a24d6b92485
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2025-03-16 18:01:53 -0400

    Add Nix file

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Rocqet: nested family polymorphism for the Rocq prover";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ ]; };        
+        coq_8_15 = pkgs.coq_8_15.override { customOCamlPackages = pkgs.ocaml-ng.ocamlPackages_4_13; };
+        ocamlPkgs = coq_8_15.ocamlPackages;
+        
+        # Make sure Coq packages use our custom Coq 8.19, which is built with OCaml 5.1
+        coqPkgs = pkgs.coqPackages_8_15.overrideScope' (self: super: {
+            coq = coq_8_15;
+        });
+
+        shellDeps = [
+          ocamlPkgs.ocaml
+          ocamlPkgs.findlib
+          pkgs.dune_3
+          coq_8_15
+          coqPkgs.flocq
+          pkgs.emacs
+          pkgs.emacsPackages.exec-path-from-shell
+          pkgs.emacsPackages.proof-general
+
+          # For bench
+          pkg.coreutils
+          pkg.gnugrep
+          pkg.gawk
+          pkg.bc
+        ];
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = shellDeps;
+            shellHook = ''
+              echo "Using OCaml version: $(ocaml -version)"
+              echo "Using Coq version: $(coqc --version)"
+            '';
+          };
+        }
+    );
+}
